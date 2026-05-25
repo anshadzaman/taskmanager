013e42e4fc3f6ca40610ce81c0001db41e765dfd
Anshad Zaman <anshadzaman10@gmail.com>
2026-05-25 13:43:57 +0530

    hello

--- src/main/java/com/anshad/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/anshad/taskmanager/TaskmanagerApplication.java
@@ -10,4 +10,8 @@ public class TaskmanagerApplication {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
 
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
